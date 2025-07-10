e0bd1d09f84ab2c65809cc75bf531c26054c3f89
austinglawe <129121412+austinglawe@users.noreply.github.com>
2025-07-10 09:50:28 -0700

    Create Overview.py

---
+++ Education/Coding/Python/Python_Basics/Overview.py
@@ -0,0 +1,46 @@
+# -----------------------------------------
+# Python_Basics:
+# Introduction to syntax, variables, control flow, functions
+# -----------------------------------------
+#
+# Comments:
+#   # This is a comment
+#
+# Variables and types:
+#   name = "Austin"        # str
+#   age = 30               # int
+#   height = 6.5           # float
+#   is_student = False     # bool
+#
+# Control flow:
+#   if age > 18:
+#       print("Adult")
+#   elif age == 18:
+#       print("Just turned adult")
+#   else:
+#       print("Minor")
+#
+# Loops:
+#   for i in range(5):
+#       print(i)
+#
+#   count = 0
+#   while count < 5:
+#       print(count)
+#       count += 1
+#
+# Functions:
+#   def greet(name):
+#       return f"Hello, {name}!"
+#
+# Modules:
+#   import math
+#   print(math.sqrt(16))
+#
+# Best practices:
+# - Meaningful names
+# - Clear indentation
+# - Use functions to organize code
+# - Comment complex logic
+#
+# -----------------------------------------
