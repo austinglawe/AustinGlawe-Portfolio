446b86afd01bc400d975aabfd3da40b77650b8a5
austinglawe <129121412+austinglawe@users.noreply.github.com>
2025-08-13 23:59:22 -0500

    Create 4.10-Nested_Loops.py

---
+++ Coursework_Examples/Code_with_Mosh/Complete_Python_Mastery/4.10-Nested_Loops.py
@@ -0,0 +1,6 @@
+for x in range(5):
+    for y in range(3):
+        print(f"({x}, {y})")
+
+# outer and inner loops
+# the inner loop is a child of the outer loop, it goes through all of the inner loop before going to the next in the outer loop.
