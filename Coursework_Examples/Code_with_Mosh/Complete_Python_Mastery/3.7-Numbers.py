636748d8405c22549b18d09f369f59633f94206b
austinglawe <129121412+austinglawe@users.noreply.github.com>
2025-08-13 23:09:46 -0500

    Create 3.7-Numbers.py

---
+++ Coursework_Examples/Code_with_Mosh/Complete_Python_Mastery/3.7-Numbers.py
@@ -0,0 +1,23 @@
+# 3 types of numbers
+# Integers
+x = 1
+# Floats
+x = 2.2
+# Complex Numbers - a + bi
+x = 1 + 2j
+
+# Standard Arithmetic operators
+print(10 + 3) # Addition
+print(10 - 3) # Subtraction
+print(10 * 3) # Mulitplication
+print(10 / 3) # Division (floating point number returned - float)
+print(10 // 3) # Division (whole number returned - integer)
+print(10 % 3) # Modulus (returns a remainder of division)
+print(10 ** 3) # Exponent
+
+# There are also augmented operators, for instance
+x = 10
+x = x + 3
+# you can do what is above or you can write the x = x + 3 like this below:
+x += 3
+
