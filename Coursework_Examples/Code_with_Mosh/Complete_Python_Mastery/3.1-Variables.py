30675851bea8a8f77781384ab9870aa557de1ad6
austinglawe <129121412+austinglawe@users.noreply.github.com>
2025-08-13 22:29:25 -0500

    Create 3.1-Variables.py

---
+++ Coursework_Examples/Code_with_Mosh/Complete_Python_Mastery/3.1-Variables.py
@@ -0,0 +1,5 @@
+students_count = 1000 # An integer - whole number
+rating = 4.99 # A Float (floating point number) - decimal number values
+is_published = False # Boolean Value - True or False (capitalized)
+course_name = "python programming" # string - need to be surrounded by quotations
+print(students_count)
