4cf31e30bcae7de6aa7f34d794444c7a9dd92f9f
austinglawe <129121412+austinglawe@users.noreply.github.com>
2025-08-13 23:34:59 -0500

    Create 4.2-Conditional_Statements.py

---
+++ Coursework_Examples/Code_with_Mosh/Complete_Python_Mastery/4.2-Conditional_Statements.py
@@ -0,0 +1,16 @@
+temperature = 35
+# if statements need a colon - indendation is required
+if temperature > 30:
+    print("It's warm")
+    print("Drink water")
+print("Done")
+
+temperature = 15
+if temperature > 30:
+    print("It's warm")
+    print("Drink water")
+elif temperature > 20:
+    print("It's nice")
+else:
+    print("It's cold")
+print("Done")
