db3084c7a713402b8f716a260770da520c41fd2b
austinglawe <129121412+austinglawe@users.noreply.github.com>
2025-08-13 22:50:04 -0500

    Create 3.4-Escape_Sequences.py

---
+++ Coursework_Examples/Code_with_Mosh/Complete_Python_Mastery/3.4-Escape_Sequences.py
@@ -0,0 +1,15 @@
+# Invalid string
+course = "Python "Programming"
+
+# could use single quotes
+course = 'Python "Programming'
+
+# could use an escape character "\" - escape the character after
+
+course = "Python "\Programming"
+
+# different escape characters to be aware of.
+# \" --- for a double quote
+# \' --- for a single quote
+# \\ --- for a backslash
+# \n --- new line
