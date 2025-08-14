21e1276d1402bf44a4b6cb603b3b3025fbb148fc
austinglawe <129121412+austinglawe@users.noreply.github.com>
2025-08-13 23:26:50 -0500

    Create 4.1-Comparison_Operators.py

---
+++ Coursework_Examples/Code_with_Mosh/Complete_Python_Mastery/4.1-Comparison_Operators.py
@@ -0,0 +1,11 @@
+# 10 > 3 --> True
+# 10 >= 3 --> True
+# 10 < 20 --> True
+# 10 <= 20 --> True
+# 10 == 10 --> True
+# 10 == "10" --> False # False because they are different data types.
+# 10 != "10" --> True
+# "bag" > "apple" --> True # True because when we sort the two words, 'bag' comes after 'apple' (b is greater than a)
+# "bag" == "BAG" --> False
+# ord("b") --> 98
+# ord("B") --> 66
