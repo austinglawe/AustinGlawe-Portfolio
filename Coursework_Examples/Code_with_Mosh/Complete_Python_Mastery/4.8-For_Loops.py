688b9710a795526d8a0cf52767066a119458dcdc
austinglawe <129121412+austinglawe@users.noreply.github.com>
2025-08-13 23:53:49 -0500

    Create 4.8-For_Loops.py

---
+++ Coursework_Examples/Code_with_Mosh/Complete_Python_Mastery/4.8-For_Loops.py
@@ -0,0 +1,18 @@
+print("Sending a message")
+# Use loops to create repetition
+for number in range(3):
+    print("Attempt")
+
+for number in range(3):
+    print("Attempt", number)
+
+
+for number in range(3):
+    print("Attempt", number + 1, (number + 1) * ".")
+
+
+for number in range(1, 4):
+    print("Attempt", number, number * ".")
+
+for number in range(1, 10, 2):
+    print("Attempt", number, number * ".")
