71c8ba17ef8cdb0cc66b371e87d3219373fac059
austinglawe <129121412+austinglawe@users.noreply.github.com>
2025-08-13 22:35:03 -0500

    Create 3.2-Variable_Names.py

---
+++ Coursework_Examples/Code_with_Mosh/Complete_Python_Mastery/3.2-Variable_Names.py
@@ -0,0 +1,12 @@
+students_count = 1000
+rating = 4.99
+is_published = False
+course_name = "python programming"
+
+
+# What 4 things do you notice about all of these variables?
+# 1. They are all descriptive and meaningful names.
+# 2. Lowercase letters are used to name variables. No capital letters.
+# 3. Used an underscore to separate multiple words. Spaces are not allowed for variables.
+# 4. Spaces on each side of the equal sign.
+# Make the code look pretty.
