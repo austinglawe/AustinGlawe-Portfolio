4665eb7417fd17ef1cecc581f19c172dbc304a5f
austinglawe <129121412+austinglawe@users.noreply.github.com>
2025-08-13 23:49:41 -0500

    Create 4.6-Chaining_Comparison_Operators.py

---
+++ Coursework_Examples/Code_with_Mosh/Complete_Python_Mastery/4.6-Chaining_Comparison_Operators.py
@@ -0,0 +1,8 @@
+# age should be between 18 and 65
+age = 22
+if age >= 18 and age < 65:
+    print("Eligible")
+
+# in math you would write the equation like this (and it can be used in python):
+if 18 <= age < 65
+# this is called chaining operators
