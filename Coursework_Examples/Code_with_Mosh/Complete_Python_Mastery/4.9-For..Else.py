fb23c075356d53f06503b08bb405b1f492b89bfc
austinglawe <129121412+austinglawe@users.noreply.github.com>
2025-08-13 23:56:36 -0500

    Create 4.9-For..Else.py

---
+++ Coursework_Examples/Code_with_Mosh/Complete_Python_Mastery/4.9-For..Else.py
@@ -0,0 +1,16 @@
+successful = True
+for number in range(3):
+    print("Attempt")
+    if successful:
+        print("Successful")
+        break
+
+
+successful = False
+for number in range(3):
+    print("Attempt")
+    if successful:
+        print("Successful")
+        break
+else:
+    print("Attempted 3 times and failed")
