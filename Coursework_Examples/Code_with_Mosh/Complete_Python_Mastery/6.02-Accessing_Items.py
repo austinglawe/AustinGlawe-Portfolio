91d6b6915b15ef6987937f6b850f4d8b2620d602
austinglawe <129121412+austinglawe@users.noreply.github.com>
2025-08-14 23:48:04 -0500

    Create 6.02-Accessing_Items.py

---
+++ Coursework_Examples/Code_with_Mosh/Complete_Python_Mastery/6.02-Accessing_Items.py
@@ -0,0 +1,12 @@
+letters = ["a", "b", "c", "d"]
+print(letters[0]) # returns first item of the list
+print(letters[-1]) # returns the last item of the list
+print(letters)
+letters[0] = "A" # changes the first indexed item of the list
+print(letters)
+print(letters[0:3])
+print(letters[:3])
+print(letters[0:])
+print(letters[:])
+print(letters[::2]) # can use a step by adding a colon
+print(letters[::-1]) # returns all items in reverse order
