b932934b8672655d2ad4e8915ae71482d98e23f3
austinglawe <129121412+austinglawe@users.noreply.github.com>
2025-08-14 23:43:04 -0500

    Create 6.01-Lists.py

---
+++ Coursework_Examples/Code_with_Mosh/Complete_Python_Mastery/6.01-Lists.py
@@ -0,0 +1,22 @@
+# Lists are in square brackets like []
+letters = ["a", "b", "c"]
+
+# 2-dimensional list is a matrix
+matrix = [[0, 1], [2, 3]]
+
+zeros = [0] * 100
+print(zeros)
+
+
+letters = ["a", "b", "c"]
+zeros = [0] * 5
+combined = zeros + letters
+
+
+# list function
+numbers = list(range(20))
+print(numebrs) # 20 not included
+
+chars = list("Hello World")
+print(chars)
+print(len(chars))
