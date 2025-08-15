5cbe43c4d3c00934d7a617edda1dfb66179d57c0
austinglawe <129121412+austinglawe@users.noreply.github.com>
2025-08-14 22:43:07 -0500

    Create 5.4-Keyword_Arguments.py

---
+++ Coursework_Examples/Code_with_Mosh/Complete_Python_Mastery/5.4-Keyword_Arguments.py
@@ -0,0 +1,19 @@
+def increment(number, by):
+    return number + by
+
+result = increment(2, 1)
+print(result)
+
+
+
+def increment(number, by):
+    return number + by
+
+print(increment(2, 1))
+
+
+# to make it more readable:
+print(increment(2, by=1))
+
+# by=1 is a keyword argument
+
