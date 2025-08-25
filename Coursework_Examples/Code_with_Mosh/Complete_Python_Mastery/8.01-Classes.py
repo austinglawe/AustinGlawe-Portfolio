f41ae87f9498ac20dba3266b920df9033b781fe4
austinglawe <129121412+austinglawe@users.noreply.github.com>
2025-08-24 23:32:48 -0500

    Create 8.01-Classes.py

---
+++ Coursework_Examples/Code_with_Mosh/Complete_Python_Mastery/8.01-Classes.py
@@ -0,0 +1,32 @@
+numbers = [1, 2]
+
+# a class is a blueprint for creating a new object
+
+
+x = 1
+print(type(x))
+# <class 'int'>
+
+# Class: blueprint for creating new objects
+# Object: instance of a class
+
+# Class: Human
+# Objects: John, Mary, Jack
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
