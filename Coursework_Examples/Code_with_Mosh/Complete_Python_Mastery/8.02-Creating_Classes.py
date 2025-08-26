c9e93514b32e50a73fbd63f18530f75d35969b14
austinglawe <129121412+austinglawe@users.noreply.github.com>
2025-08-25 22:55:21 -0500

    Create 8.02-Creating_Classes.py

---
+++ Coursework_Examples/Code_with_Mosh/Complete_Python_Mastery/8.02-Creating_Classes.py
@@ -0,0 +1,24 @@
+# PascalNamingConvention 
+class Point:
+    def draw(self):
+        print("draw")
+
+point = Point()
+print(type(point)) # <class '__main__.Point'>
+print(isinstance(point, Point))
+print(isinstance(point, int))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
